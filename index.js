1787c4d18653f478a2528060357a8c9e02a399b6
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-05-01 11:35:11 -1000

    added procfile

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '/style.css'))
+});
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/script.js'))
+})
+
+const port = process.env.PORT || 4007;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
